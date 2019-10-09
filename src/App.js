1e19e132408179957a5b919c983b7b3b42a00144
Thiago Galvani <thiagopaiva99@gmail.com>
2019-10-09 15:15:16 -0300

    Styling the header and rendering at the app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import GlobalStyles from './styles/global'
+import Header from './components/Header';
 
 function App() {
   return (
     <main>
       <GlobalStyles />
+
+      <Header />
     </main>
   );
 }
