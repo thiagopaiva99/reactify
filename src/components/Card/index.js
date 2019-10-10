e633b804202eea8c9511c3f897982aa9b2d57781
Thiago Galvani <thiagopaiva99@gmail.com>
2019-10-10 10:31:28 -0300

    Creating the hook to drop the card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { If, For } from 'react-extras';
-import { useDrag } from 'react-dnd'
+import { useDrag, useDrop } from 'react-dnd'
 
 import { Container, Label } from './styles';
 
 export default function Card({ data: card }) {
+  const ref = useRef()
+
   const [{ isDragging }, dragRef] = useDrag({
     item: {
       type: 'CARD'
@@ -14,8 +16,17 @@ export default function Card({ data: card }) {
     })
   })
 
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover: (item, monitor) => {
+      
+    }
+  })
+
+  dragRef(dropRef(ref))
+
   return (
-    <Container ref={dragRef} isDragging={isDragging}>
+    <Container ref={ref} isDragging={isDragging}>
       <header>
         <For of={card.labels} render={label => <Label key={label} color={label} /> } />
       </header>
